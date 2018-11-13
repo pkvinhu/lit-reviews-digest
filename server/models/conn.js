9c3724f01c89686ca911fb8b20fa50b4b83560b4
Kevin Hu <pkvinhu@gmail.com>
2018-11-13 07:01:39 -0500

    linting

--- server/models/conn.js
+++ server/models/conn.js
@@ -14,5 +14,6 @@ if (!global.hasOwnProperty('db')) {
   } else {
 	conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/stackathon1', { logging: false });
 }
+}
 
 module.exports = conn;
