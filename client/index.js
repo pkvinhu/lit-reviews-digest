5a4d3bd90b11f95249fcb727d6df8d18a22ff4c9
Kevin Hu <pkvinhu@gmail.com>
2018-11-12 19:31:36 -0500

    package.json update

--- client/index.js
+++ client/index.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import Home from './Home';
 import { Provider } from 'react-redux';
 import { store } from './store'
 
 render(
+<Fragment>
   <Provider store={store}>
     <Home />
-  </Provider>,
+  </Provider>
+</Fragment>,
   document.getElementById('app')
 )
