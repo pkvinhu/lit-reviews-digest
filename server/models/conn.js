be30da6ca322506ad8888e1044bfa6582005767f
Kevin Hu <pkvinhu@gmail.com>
2018-11-13 07:05:40 -0500

    heroku

--- server/models/conn.js
+++ server/models/conn.js
@@ -1,19 +1,5 @@
-if (!global.hasOwnProperty('db')) {
-  var Sequelize = require('sequelize')
-    , conn = null
+const Sequelize = require('sequelize')
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/stackathon1', { logging: false });
 
-  if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
-    // the application is executed on Heroku ... use the postgres database
-    conn = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
-      dialect:  'postgres',
-      protocol: 'postgres',
-      port:     match[4],
-      host:     match[3],
-      logging:  true //false
-    })
-  } else {
-	conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/stackathon1', { logging: false });
-}
-}
 
 module.exports = conn;
