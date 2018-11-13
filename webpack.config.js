209900d593e7033f6821eae191888a2eec8ff1a0
Kevin Hu <pkvinhu@gmail.com>
2018-11-12 20:32:29 -0500

    updte

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ module.exports = {
   	  {
   	  	test:/\.js$/,
   	  	loader: 'babel-loader',
-  	  	exclude: /node_modules/
+  	  	exclude: /node_modules/,
+        options: {
+          babelrc: path.join(process.cwd(), './babelrc')
+        }
   	  }
   	]
   }
